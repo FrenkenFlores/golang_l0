9d1590511f9eef5782f84281e8f547181fb05ffa
FrenkenFlores <saifualdin.evloev@gmail.com>
2025-01-05 12:59:13 +0300

    update #3: add configs
    Signed-off-by: FrenkenFlores <saifualdin.evloev@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,21 @@ import (
 	"log"
 
 	gol0 "github.com/FrenkenFlores/golang_l0"
+	"github.com/spf13/viper"
 )
 
+func initConfigs() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
+
 func main() {
-	port := "8080"
+	if err := initConfigs(); err != nil {
+		log.Fatalf("Erorr occurred while reading configs: %s", err.Error())
+		return
+	}
+	port := viper.GetString("port")
 	if err := gol0.NewServer(port); err != nil {
 		log.Fatalf("Error occurred while setting up the server: %s", err.Error())
 	}
